701afe75ef60f829db5477e91a68cc521854e1c8
lundg <id23oln@cs.umu.se>
2025-03-19 09:51:13 +0100

    småfix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,47 +1,10 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import LoggaIn from './loggaIn.jsx';
 class Header extends React.Component {
 
     render() {
         return (
-            <header>
-
-                <nav className="navboard">
-                    <div className="lock">
-                        <span className="material-symbols-outlined">lock</span>
-
-                    </div>
-                    <div id="logo">
-                        <Link to="/">
-                            <img src="/src/images/Icon.png"
-                                 alt="Det här är en bild på Step in loggan"/> </Link>
-                    </div>
-                    <button className="hamburger">
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                    </button>
-
-                    <div className="Navlist">
-                        <ul>
-                            <li><Link to="/ourGyms"> VÅRA GYM</Link></li>
-                            <li><Link to="/train"> TRÄNA</Link></li>
-                            <li><Link to=""> FÖRETAG</Link></li>
-                            <li>
-                                <LoggaIn></LoggaIn>
-                            </li>
-                            <li>
-                                <button className=" orangeButton" onClick={() => window.location.to = 'bli-medlem.html'}>
-                                    BLI MEDLEM
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-
-                </nav>
-
-            </header>
+            <LoggaIn />
         );
     }
 }
--- src/components/loggaIn.jsx
+++ src/components/loggaIn.jsx
@@ -1,4 +1,5 @@
-import {useState} from "react";
+import  {useState} from "react";
+import {Link} from "react-router-dom";
 
 
 
@@ -9,14 +10,52 @@ const LoggaInRuta = () => {
     };
     return (
         <>
-          <button className="logga-in" id="logga-in" onClick={handleShow}>Logga in</button>
-            <div id="logga-inRuta" className={`logga-inRuta shadow ${show ? "show" : ""}`}>
-                <form>
-                    <input type="email" name="E-post" id="logga-inEpost" placeholder="E-postadress"/>
-                    <input type="password" name="password" id="logga-inPass" placeholder="Password"/>
-                    <input type="submit" value="Logga in" id="logga-inSubmit"/>
-                </form>
-            </div>
+
+            <header className={`${show ? "visa" : ""}`}>
+
+                <nav className="navboard">
+                    <div className="lock">
+                        <span className="material-symbols-outlined">lock</span>
+
+                    </div>
+                    <div id="logo">
+                        <Link to="/">
+                            <img src="/src/images/Icon.png"
+                                 alt="Det här är en bild på Step in loggan"/> </Link>
+                    </div>
+                    <button className="hamburger">
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </button>
+
+                    <div className="Navlist">
+                        <ul>
+                            <li><Link to="/ourGyms"> VÅRA GYM</Link></li>
+                            <li><Link to="/train"> TRÄNA</Link></li>
+                            <li><Link to=""> FÖRETAG</Link></li>
+                            <li>
+                                <button className="logga-in" id="logga-in" onClick={handleShow}>Logga in</button>
+                                <div id="logga-inRuta" className={`logga-inRuta shadow ${show ? "show" : ""}`}>
+                                    <form>
+                                        <input type="email" name="E-post" id="logga-inEpost" placeholder="E-postadress"/>
+                                        <input type="password" name="password" id="logga-inPass" placeholder="Password"/>
+                                        <input type="submit" value="Logga in" id="logga-inSubmit"/>
+                                    </form>
+                                </div>
+                            </li>
+                            <li>
+                                <button className=" orangeButton" onClick={() => window.location.to = 'bli-medlem.html'}>
+                                    BLI MEDLEM
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
+
+
+            </header>
         </>
     )
 };
